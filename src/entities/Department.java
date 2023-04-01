66244c0e65b5f7d4969c1b2f28e04cca01d55735
Arthur Bem <arthurcbem@gmail.com>
2023-04-01 15:54:10 -0300

    Seller entity class

--- src/entities/Department.java
+++ src/entities/Department.java
@@ -2,63 +2,64 @@ package entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable{
-    private static final long serialVersionUID = 1L;
-    private Integer Id;
-    private String Name;
+public class Department implements Serializable {
 
-    public Department(){
-}
+	private static final long serialVersionUID = 1L;
 
-    public Department(Integer id, String name) {
-        Id = id;
-        Name = name;
-    }
+	private Integer id;
+	private String name;
+	
+	public Department() {
+	}
 
-    public Integer getId() {
-        return Id;
-    }
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public void setId(Integer id) {
-        Id = id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getName() {
-        return Name;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        Name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
-        return result;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Department other = (Department) obj;
-        if (Id == null) {
-            if (other.Id != null)
-                return false;
-        } else if (!Id.equals(other.Id))
-            return false;
-        return true;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return "Department [Id=" + Id + ", Name=" + Name + "]";
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
 }
